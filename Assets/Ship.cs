14cc3e401006d61788dd6a28d5b32ffe13ee5b14
Jay Peterson <jaypeterson47@live.com>
2019-05-20 23:28:08 -0500

    Added second level Refined level set up Set up working game loop Added new sounds Changed old thruster sound.

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -1,47 +1,122 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ship : MonoBehaviour
 {
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float rscMainThrust = 100f;
+	[SerializeField] AudioClip mainEngine;
+	[SerializeField] AudioClip death;
+	[SerializeField] AudioClip finish;
+
 	Rigidbody rigidBody;
-	AudioSource thrusterSound;
+	AudioSource audioSource;
+
+	enum State {Alive, Dying, Transcending};
+	State state = State.Alive;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rigidBody = GetComponent<Rigidbody>();
-		thrusterSound = GetComponent<AudioSource>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		ProcessInput();
+		if (state == State.Alive)
+		{
+			HandleThrustInput();
+			HandleRotationInput();
+		}
 	}
 
-	private void ProcessInput()
+	void OnCollisionEnter(Collision collision)
+	{
+		if (state != State.Alive)
+		{
+			return;
+		}
+
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				break;
+			case "Finish":
+				StartFinishSequence();
+				break;
+			default:
+				StartDeathSequence();
+				break;
+		}
+	}
+
+	private void StartFinishSequence()
+	{
+		state = State.Transcending;
+		rigidBody.constraints = RigidbodyConstraints.FreezeAll;
+		audioSource.Stop();
+		audioSource.PlayOneShot(finish);
+		Invoke("LoadNextScene", 1f);
+	}
+
+	private void StartDeathSequence()
+	{
+		state = State.Dying;
+		audioSource.Stop();
+		audioSource.PlayOneShot(death);
+		Invoke("LoadFirstLevel", 1);
+	}
+
+	private void LoadNextScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	private void LoadFirstLevel()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	private void HandleThrustInput()
 	{
 		if (Input.GetKey(KeyCode.Space))	//Able to thrust while rotating
 		{
-			if (thrusterSound.isPlaying == false)
-			{
-				thrusterSound.Play();
-			}
-			rigidBody.AddRelativeForce(Vector3.up);
+			ApplyThrust();
 		}
+		else
+		{
+			audioSource.Stop();
+		}
+	}
+
+	private void ApplyThrust()
+	{
+		if (audioSource.isPlaying == false)
+		{
+			audioSource.PlayOneShot(mainEngine);
+		}
+		rigidBody.AddRelativeForce(Vector3.up * rscMainThrust);
+	}
+
+	private void HandleRotationInput()
+	{
+		rigidBody.freezeRotation = true;	//control rotation manually
+
+		float frameRotation = rcsThrust * Time.deltaTime;
+
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * frameRotation);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(Vector3.back);
+			transform.Rotate(Vector3.back * frameRotation);
 		}
 
-		if(Input.GetKeyUp(KeyCode.Space))
-		{
-			thrusterSound.Stop();
-		}
+		rigidBody.freezeRotation = false;	//control rotation with physics
 	}
 }
